42341a4612cd94f6674b81446e79af1f14db4f4d
sunjae0902 <sunjaegg0902@naver.com>
2024-11-03 15:20:15 +0900

    add: Color Scheme

---
+++ mystagram/mystagram/AppColor/AppIColor.swift
@@ -0,0 +1,31 @@
+//
+//  AppColor.swift
+//  mystagram
+//
+//  Created by sunjae on 11/3/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let gradientYellow = Color(hex: "#FFF9F1")
+    static let gradientRed = Color(hex: "#FDF2F6")
+    static let gradientBlue = Color(hex: "#EDF6FF")
+    static let gradientGreen = Color(hex: "#EEFBF2")
+}
+
+extension Color {
+    init(hex: String) {
+         let scanner = Scanner(string: hex)
+         _ = scanner.scanString("#")
+         
+         var rgb: UInt64 = 0
+         scanner.scanHexInt64(&rgb)
+         
+         let r = Double((rgb >> 16) & 0xFF) / 255.0
+         let g = Double((rgb >>  8) & 0xFF) / 255.0
+         let b = Double((rgb >>  0) & 0xFF) / 255.0
+         self.init(red: r, green: g, blue: b)
+       }
+}
+
